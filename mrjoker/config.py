4f83bffe519cad6578a778962c284db087add00f
ThaniOruvan25 <89347874+ThaniOruvan25@users.noreply.github.com>
2021-09-04 19:53:22 +0530

    Update and rename sample_config.py to config.py

--- mrjoker/config.py
+++ mrjoker/config.py
@@ -14,40 +14,40 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID= 126456 # integer value, dont use ""
-    API_HASH= "0"
-    TOKEN = "0"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1131653685   # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = ""
-    SUPPORT_CHAT = "lkhitech"  # Your own group for support, do not add the @
+    API_ID= 7763257 # integer value, dont use ""
+    API_HASH= "52c9cbf4b4ee78eda09eb3d9ac0673d7"
+    TOKEN = "1982719959:AAFBzcShmpSb1SdXho3vFXlqvCW232NhXlY"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 1441767180   # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "MrJackOwnerbot"
+    SUPPORT_CHAT = "MrJackOwnerbot"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -100121564564
+        -1001558400787
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -100123456464
+         -1001558400787
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = ""  
+    SQLALCHEMY_DATABASE_URI = "postgres://ypunjmvfuxdhgl:782efc8464c14d344989b8213fd0a22c8bf31a348bb811fa4e407ec6a7d29bca@ec2-18-235-45-217.compute-1.amazonaws.com:5432/ddinfnb3br5n1k"  
     REDIS_URI = " "
     LOAD = []
     NO_LOAD = ["rss", "cleaner", "connection", "math"]
     WEBHOOK = False
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
+    SPAMWATCH_API = "YT5pImB5XKNI8g5Pz4ze50C0GsZnHPUowDyXcnjRZMhdVG7ytwNvtg76L2FHUcTc"  # go to support.spamwat.ch to get key
     SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
 
     # OPTIONAL
     ##List of id's -  (not usernames) for users which have sudo access to the bot.
-    DRAGONS = "1131653685"
+    DRAGONS = "1441767180"
     ##List of id's - (not usernames) for developers who will have the same perms as the owner
-    DEV_USERS = "1131653685"
+    DEV_USERS = "1441767180"
     ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    DEMONS = "1131653685"
+    DEMONS = "1441767180"
     # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    TIGERS = "1131653685"
-    WOLVES = "1131653685"
+    TIGERS = "1441767180"
+    WOLVES = "1441767180"
     DONATION_LINK = None  # EG, paypal
     CERT_PATH = None
     PORT = 5000
@@ -59,9 +59,9 @@ class Config(object):
     BAN_STICKER = ""  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
     ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
     CASH_API_KEY = (
-        ""  # Get your API key from https://www.alphavantage.co/support/#api-key
+        "GJIR9QTSA0678TH3"  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
-    TIME_API_KEY = ""  # Get your API key from https://timezonedb.com/api
+    TIME_API_KEY = "ZBL5HYQZYTYZ"  # Get your API key from https://timezonedb.com/api
     WALL_API = (
         "xyz"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
     )
